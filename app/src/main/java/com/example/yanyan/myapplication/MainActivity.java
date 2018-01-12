adf67a67e63719a8669243bfbd76357e55914c75
yanyan <1716330643@qq.com>
2018-01-12 14:31:47 +0800

    first commit

---
+++ app/src/main/java/com/example/yanyan/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.yanyan.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.igexin.sdk.PushManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // com.getui.demo.DemoPushService 为第三方自定义推送服务
+        PushManager.getInstance().initialize(this.getApplicationContext(), com.example.yanyan.myapplication.DemoPushService.class);
+        // com.getui.demo.DemoIntentService 为第三方自定义的推送服务事件接收类
+        PushManager.getInstance().registerPushIntentService(this.getApplicationContext(), com.example.yanyan.myapplication.DemoIntentService.class);
+
+        PushManager.getInstance().setHeartbeatInterval(this.getApplicationContext(),5);
+    }
+}
